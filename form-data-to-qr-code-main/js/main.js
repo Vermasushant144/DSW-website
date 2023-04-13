2b9962b3bfd2ea108a2723c8c1a4811605cd627b
sushant verma <98468495+Vermasushant144@users.noreply.github.com>
2023-04-13 20:10:06 +0530

    Add files via upload

---
+++ form-data-to-qr-code-main/js/main.js
@@ -0,0 +1,87 @@
+(function($) {
+
+	"use strict";
+
+
+  // Form
+	var contactForm = function() {
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					branch: "required",
+					club: "required",
+					erpid: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					year: {
+						required: true,
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					branch: "Please enter your branch",
+					erpid: " Please  enter your ERP ID",
+					year: "Please enter your year",
+					club: "Please enter club name",
+				},
+				/* submit via ajax */
+				
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/sendEmail.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeIn();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+
+		               	setTimeout(function(){
+				               $('#form-message-success').fadeOut();   
+		               	}, 8000);
+
+		               	setTimeout(function(){
+				               $submit.css('display', 'none').text(waitText);  
+		               	}, 1400);
+
+		               	setTimeout(function(){
+		               		$( '#contactForm' ).each(function(){
+											    this.reset();
+											});
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Form Submitted Successfully .");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		} // end submitHandler
+
+			});
+		}
+	};
+	contactForm();
+
+})(jQuery);
